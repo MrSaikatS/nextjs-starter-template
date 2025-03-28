f0787367bd14905f249359b02b4a067ff8d69382
Saikat Sardar <saikatsardar5@gmail.com>
2025-03-28 20:49:34 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 const page = () => {
   return (
     <>
-      <section className="grid h-[90dvh] place-items-center">
+      <section className="grid h-[80dvh] place-items-center">
         <div className="bg-gradient-to-tr from-rose-600 to-violet-600 bg-clip-text p-2">
           <span className="text-5xl font-bold text-transparent">
             Nextjs Starter Template
