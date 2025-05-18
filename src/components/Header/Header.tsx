2eaf084f722fe7a321db72a09cecfbc177c362b6
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-18 11:35:39 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,11 +8,13 @@ const Header = ({ appName }: HeaderProps) => {
       className="border-b shadow"
       aria-label="app-header">
       <div className="container mx-auto flex items-center justify-between px-6 py-3">
-        <h1
-          className="text-2xl font-semibold"
-          aria-label="App Name">
-          {appName}
-        </h1>
+        <Link href={"/"}>
+          <h1
+            className="text-2xl font-semibold"
+            aria-label="App Name">
+            {appName}
+          </h1>
+        </Link>
 
         <nav className="flex items-center gap-4">
           <Link href={"/"}>Home</Link>
