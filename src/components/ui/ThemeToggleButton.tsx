6635ae2bae7baf6b3063a7532b3c07a07ba70ca1
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-25 14:24:14 +0530

    ThemeToggleButton animation update

--- src/components/ui/ThemeToggleButton.tsx
+++ src/components/ui/ThemeToggleButton.tsx
@@ -12,12 +12,12 @@ const ThemeToggleButton = () => {
       className="flex cursor-pointer items-center">
       <Sun
         size={28}
-        className="scale-100 opacity-100 transition-all duration-300 dark:scale-0 dark:opacity-0"
+        className="-rotate-90 opacity-100 transition-all duration-300 dark:rotate-0 dark:opacity-0"
       />
 
       <MoonStar
         size={28}
-        className="absolute scale-0 opacity-0 transition-all duration-300 dark:scale-100 dark:opacity-100"
+        className="absolute -rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100"
       />
     </button>
   );
