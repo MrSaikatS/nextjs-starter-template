4217583028f5802b345da63012483ac5c2706e94
Saikat Sardar <saikatsardar5@gmail.com>
2025-06-11 23:48:43 +0530

    ui system and project structure updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,6 @@ const page = () => {
   return (
     <>
       <section className="grid h-[80dvh] place-items-center">
-        {/* <div className="bg-gradient-to-tr from-rose-600 to-violet-600 bg-clip-text p-2">
-          <span className="text-5xl font-bold text-transparent">
-            Nextjs Starter Template
-          </span>
-        </div> */}
-
         <AuroraText className="text-5xl font-bold">
           Nextjs Starter Template
         </AuroraText>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import ThemeToggleButton from "../ui/ThemeToggleButton";
+import ThemeToggleButton from "../customui/ThemeToggleButton";
 
 const Header = () => {
   return (
