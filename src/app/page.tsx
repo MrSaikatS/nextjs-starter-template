2bcc3a1c12e1bfb79c6eaa685ba8f8568e8e93ba
Saikat Sardar <saikatsardar5@gmail.com>
2025-07-29 17:52:07 +0530

    Reformat codebase with consistent indentation
    Updated indentation across globals.css, layout.tsx, page.tsx, and aurora-text.tsx for improved code consistency and readability. No functional changes were made.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home | Nextjs Starter Template",
-  description: "Home page of Nextjs Starter Template",
+	title: "Home | Nextjs Starter Template",
+	description: "Home page of Nextjs Starter Template",
 };
 
 const page = () => {
-  return (
-    <>
-      <section className="grid h-[80dvh] place-items-center">
-        <AuroraText className="text-5xl font-bold">
-          Nextjs Starter Template
-        </AuroraText>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className="grid h-[80dvh] place-items-center">
+				<AuroraText className="text-5xl font-bold">
+					Nextjs Starter Template
+				</AuroraText>
+			</section>
+		</>
+	);
 };
 
 export default page;
